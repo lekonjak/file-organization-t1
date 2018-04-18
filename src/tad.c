4b1b6971c606bd2310f63862efdb51c8b9829f73
Tiago Triques <tiagotriques@gmail.com>
2018-04-18 15:20:09 -0300

    Removed &s from fread
    Still not reading all the 'ouput.dat' file :/

--- src/tad.c
+++ src/tad.c
@@ -2,104 +2,166 @@
 #include "utils.h"
 
 /* PDF diz: 'registro de 87 bytes'
- * Precisamos "alinhar" com o tamanho de página de disco 
+ * Precisamos "alinhar" com o tamanho de página de disco
  * (veja https://0x0.st/6C - slide 36)
  *
  * Uma página tem 2^12 = 4096 bytes, então precisamos deixar cada registro com
  * 2^7 = 128 bytes. Teremos 2^(12 - 7) = 2^5 = 32 registros por página de disco
  *
+ * Ou não, se alguém souber me avisa
  */
 struct registro {
-	// Campos de tamanho fixo
-	int codINEP;		//4 bytes
-	char dataAtiv[10];  //10 bytes
-	char uf[2]; 		//2 bytes
+    // Campos de tamanho fixo
+    int codINEP;		//4 bytes
+    char dataAtiv[11];  //10 bytes
+    char uf[3]; 		//2 bytes
 
-	//Campos de tamanho variável
-	char *nomeEscola;   //8 bytes
-	char *municipio;	//8 bytes
-	char *prestadora;	//8 bytes
+    //Campos de tamanho variável
+    char *nomeEscola;
+    char *municipio;
+    char *prestadora;
 };
 
 struct header {
-	char status;
-	int stackTop; //🔝
+    char status;
+    int stackTop; //🔝
 };
 
-/*
- * Especificação do csv:
- * prestadora	dataAtiv	codINEP		nomeEscola				municipio	uf
- * CTBC;		18/09/2009;	31031917;	EM PERCILIA LEONARDO;	ARAUJOS;	MG
+/* A função lê uma linha de cada vez do arquivo csv, separa cada campo, e escreve os dados
+ * em outro arquivo binário. Uma mensagem de erro é printada caso o arquivo de entrada e/ou saída
+ * não consiga ser aberto.
+ *
+ * A função freadline (utils.h) retorna a linha lida do arquivo de entrada
+ * A função split (utils.h) separa cada um dos campos da linha com strsep (man 3 strsep)
+ * Ps: strsep é mais "robusta" que strtok pois suporta delimitadores consecutivos.
+ * No arquivo de entrada, ";;" indica um campo "null". Usando strtok os campos 'null' seriam ignorados
  */
 void csv2bin(char *filename) {
-	fprintf(stderr, "Nome do arquivo para abrir: >%s<\n", filename);
+    FILE *infile = NULL, *outfile = NULL;
+    char *linha = NULL;
+    char **fields = NULL;
+    int regSize = 0;
+    Registro r = {0};
+    Header h = {0};
 
-	FILE *infile = NULL, *outfile = NULL;
+    infile = fopen(filename, "r");
 
-	infile = fopen(filename, "r");
+    if(infile == NULL) {
+        fprintf(stdout, "Falha no carregamento do arquivo\n");
+        return;
+    }
 
-	if(infile == NULL) {
-		fprintf(stdout, "Falha no carregamento do arquivo\n");
-		return;
-	}
-	
-	outfile = fopen("output.dat", "w");
+    outfile = fopen("output.dat", "wb");
 
-	if(outfile == NULL) {
-		fprintf(stdout, "Falha no carregamento do arquivo\n");
-		return;
-	}
-		
-	char *string = NULL;
-	char **fields = NULL;
+    if(outfile == NULL) {
+        fprintf(stdout, "Falha no carregamento do arquivo\n");
+        return;
+    }
 
-	while(!feof(infile)) {
-		string = freadline(infile);
+    h.status = 0;
+    h.stackTop = -1;
 
-		//freadline returns null if it is EOF
-		if(string != NULL) {
-			fields = split(string);
+    //Write header
+    fwrite(&h.status, sizeof(char), 1, outfile);
+    fwrite(&h.stackTop, sizeof(int), 1, outfile);
 
-            for(int i = 0; i < 6; i++)
-                printf("fields[%d] = [%s]\n", i, fields[i]);
-        
+    while(!feof(infile)) {
+        linha = freadline(infile);
+
+        //freadline returns null if it is EOF
+        if(linha != NULL) {
+            fields = split(linha);
+
+            //Reg size
+            regSize = strlen(fields[0]) + strlen(fields[3]) + strlen(fields[4]);
+            regSize += 10+2+4+4+4;
+
+            //TODO Melhorar essa djabo
+            //TODO 2: Registro tem tamanho fixo, precisamos garantir que estamos
+            //escrevendo 87 bytes toda vez.
+            //TODO 3: Campos NULL:
+            //  - tamanho fixo: preencher tudo com 0
+            //  - tamanho variável: size = 0
+            //Another gambs
+            r.prestadora = calloc(87, sizeof(char));
+            strncpy(r.prestadora, fields[0], strlen(fields[0]));
+
+            //Fixo 10 bytes
+            strncpy(r.dataAtiv, fields[1], 10);
+
+            //Fixo 4 bytes
+            r.codINEP = atoi(fields[2]);
+
+            r.nomeEscola = calloc(strlen(fields[3])+1, sizeof(char));
+            strncpy(r.nomeEscola, fields[3], strlen(fields[3]));
+
+            r.municipio = calloc(strlen(fields[4])+1, sizeof(char));
+            strncpy(r.municipio, fields[4], strlen(fields[4]));
+
+            //Fixo 2 bytes
+            strncpy(r.uf, fields[5], 2);
+
+            int aux;
+            //Write each entry
+            fwrite(&r.codINEP, sizeof(int), 1, outfile);
+            fwrite(r.dataAtiv, 10*sizeof(char), 1, outfile);
+            fwrite(r.uf, 2*sizeof(char), 1, outfile);
+
+            //Tamanho e nome da escola
+            aux = strlen(r.nomeEscola);
+            fwrite(&aux, sizeof(int), 1, outfile);
+            fwrite(r.nomeEscola, aux*sizeof(char), 1, outfile);
+
+            aux = strlen(r.municipio);
+            fwrite(&aux, sizeof(int), 1, outfile);
+            fwrite(r.municipio, aux*sizeof(char), 1, outfile);
+
+            //Gambs
+            aux = 87 - regSize;
+            fwrite(&aux, sizeof(int), 1, outfile);
+            fwrite(r.prestadora, aux*sizeof(char), 1, outfile);
+
+            free(r.prestadora);
+            free(r.nomeEscola);
+            free(r.municipio);
+
             for(int i = 0; i < 6; i++)
                 free(fields[i]);
             free(fields);
-			free(string);
+            free(linha);
         }
-	}
+    }
 
-	//Fechar arquivos de entrada e saída	
-	fclose(infile);
-	fclose(outfile);
+    //Fechar arquivos de entrada e saída
+    fclose(infile);
+    fclose(outfile);
 
-	return;
+    return;
 }
 
 void bin2out(void) {
     FILE *fp;
     Registro r;
-    int sizeEscola ,sizeMunicipio ,sizePrestadora;
-	long offset = 5;
+    int sizeEscola, sizeMunicipio, sizePrestadora;
+    long offset = 5;
 
-    fp = fopen("output.dat", "r");
+    fp = fopen("output.dat", "rb");
     fseek(fp, offset, SEEK_SET);
 
-    while(fpeek(fp)){      
+    while(fpeek(fp)){
         fread(&r.codINEP, sizeof(int), 1, fp);
-        fread(&r.dataAtiv, sizeof(char)*10, 1, fp);
-        fread(&r.uf, sizeof(char)*2, 1, fp);
+        fread(r.dataAtiv, sizeof(char)*10, 1, fp);
+        fread(r.uf, sizeof(char)*2, 1, fp);
         fread(&sizeEscola, sizeof(int), 1, fp);
-            r.nomeEscola = (char*) malloc ( sizeof(char)*sizeEscola+1);    
-        fread(&r.nomeEscola, sizeof(char)*sizeEscola, 1, fp);
+        r.nomeEscola = (char*) malloc ( sizeof(char)*sizeEscola+1);
+        fread(r.nomeEscola, sizeof(char)*sizeEscola, 1, fp);
         fread(&sizeMunicipio, sizeof(int), 1, fp);
-            r.municipio = (char*) malloc ( sizeof(char)*sizeMunicipio+1);    
-        fread(&r.municipio, sizeof(char)*sizeMunicipio, 1, fp);
+        r.municipio = (char*) malloc ( sizeof(char)*sizeMunicipio+1);
+        fread(r.municipio, sizeof(char)*sizeMunicipio, 1, fp);
         fread(&sizePrestadora, sizeof(int), 1, fp);
-            r.prestadora = (char*) malloc ( sizeof(char)*sizePrestadora+1);    
-        fread(&r.prestadora, sizeof(char)*sizePrestadora, 1, fp);
-        
+        r.prestadora = (char*) malloc ( sizeof(char)*sizePrestadora+1);
+        fread(r.prestadora, sizeof(char)*sizePrestadora, 1, fp);
+
         catReg(&r, sizeEscola, sizeMunicipio, sizePrestadora);
 
         free(r.nomeEscola);
@@ -113,7 +175,7 @@ void bin2out(void) {
 int fpeek(FILE *fp){
     char test = (char)fgetc(fp);
     ungetc( test, fp);
-    if( test == EOF) return 0;
+    if(test == EOF) return 0;
     return 1;
 }
 
@@ -121,36 +183,38 @@ void catReg(Registro *reg, int sizeEscola, int sizeMunicipio, int sizePrestadora
     fprintf(stdout,"%d %s %s %d %s %d %s %d %s\n",reg->codINEP, reg->dataAtiv, reg->uf, sizeEscola, reg->nomeEscola \
             , sizeMunicipio, reg->municipio, sizePrestadora, reg->prestadora);
 }
+
 void bin2outGrep(char *category, void *element, int (*cmp)(void *, void *)){
     FILE *fp;
     Registro r;
+
     int sizeEscola ,sizeMunicipio ,sizePrestadora, flag = 0;
 	long offset = 5;
     void *this = NULL;
     fp = fopen("output.dat", "r");
     fseek(fp, offset, SEEK_SET);
 
-    while(fpeek(fp)){       
+    while(fpeek(fp)){
         fread(&r.codINEP, sizeof(int), 1, fp);
         fread(&r.dataAtiv, sizeof(char)*10, 1, fp);
         fread(&r.uf, sizeof(char)*2, 1, fp);
         fread(&sizeEscola, sizeof(int), 1, fp);
-            r.nomeEscola = (char*) malloc ( sizeof(char)*sizeEscola+1);    
+        r.nomeEscola = (char*) malloc ( sizeof(char)*sizeEscola+1);
         fread(&r.nomeEscola, sizeof(char)*sizeEscola, 1, fp);
         fread(&sizeMunicipio, sizeof(int), 1, fp);
-            r.municipio = (char*) malloc ( sizeof(char)*sizeMunicipio+1);    
+        r.municipio = (char*) malloc ( sizeof(char)*sizeMunicipio+1);
         fread(&r.municipio, sizeof(char)*sizeMunicipio, 1, fp);
         fread(&sizePrestadora, sizeof(int), 1, fp);
-            r.prestadora = (char*) malloc ( sizeof(char)*sizePrestadora+1);    
+        r.prestadora = (char*) malloc ( sizeof(char)*sizePrestadora+1);
         fread(&r.prestadora, sizeof(char)*sizePrestadora, 1, fp);
-        
+
         this = category[0] == 'c' ? &r.codINEP :\
                 category[0] == 'd' ? &r.dataAtiv:\
                  category[0] == 'u' ? &r.uf:\
                   category[0] == 'n' ? &r.nomeEscola:\
                    category[0] == 'p' ? &r.prestadora:\
-                    category[0] == 'm' ? &r.municipio: (void*)NULL; // precompilator gave me no choice, i had to cast do avoid warning 
-        
+                    category[0] == 'm' ? &r.municipio: (void*)NULL; // precompilator gave me no choice, i had to cast do avoid warning
+
         if( this == NULL){
             fclose(fp);
             free(r.nomeEscola);
@@ -163,12 +227,12 @@ void bin2outGrep(char *category, void *element, int (*cmp)(void *, void *)){
         if(!cmp(element, this)){
             catReg(&r, sizeEscola, sizeMunicipio, sizePrestadora);
             flag++;
-        } 
+        }
 
         free(r.nomeEscola);
         free(r.municipio);
         free(r.prestadora);
-            
+
     }
 
     if(!flag) printf("Registro inexistente.\n");
@@ -187,9 +251,11 @@ int intCmp(void *a, void *b){
 int sstrCmp(void *a, void *b){
     return strcmp((char*)a, (char*)b);
 }
+
 char *strClear(char *s){
     char *out = (char*) malloc (sizeof(char)*(strlen(s)-2));
     int i;
+
     for( i = 1; s[i] != '\''; i++){
         out[i-1] = s[i];
     }
@@ -214,15 +280,15 @@ void bin2outRRN(int RRN) {
         fread(&r.dataAtiv, sizeof(char)*10, 1, fp);
         fread(&r.uf, sizeof(char)*2, 1, fp);
         fread(&sizeEscola, sizeof(int), 1, fp);
-            r.nomeEscola = (char*) malloc ( sizeof(char)*sizeEscola+1);    
+            r.nomeEscola = (char*) malloc ( sizeof(char)*sizeEscola+1);
         fread(&r.nomeEscola, sizeof(char)*sizeEscola, 1, fp);
         fread(&sizeMunicipio, sizeof(int), 1, fp);
-            r.municipio = (char*) malloc ( sizeof(char)*sizeMunicipio+1);    
+            r.municipio = (char*) malloc ( sizeof(char)*sizeMunicipio+1);
         fread(&r.municipio, sizeof(char)*sizeMunicipio, 1, fp);
         fread(&sizePrestadora, sizeof(int), 1, fp);
-            r.prestadora = (char*) malloc ( sizeof(char)*sizePrestadora+1);    
+            r.prestadora = (char*) malloc ( sizeof(char)*sizePrestadora+1);
         fread(&r.prestadora, sizeof(char)*sizePrestadora, 1, fp);
-      
+
         catReg(&r, sizeEscola, sizeMunicipio, sizePrestadora);
 
         free(r.nomeEscola);
@@ -231,7 +297,7 @@ void bin2outRRN(int RRN) {
     }else{
         printf("Registro inexistente.\n");
     }
-    
+
     fclose(fp);
     return;
 
